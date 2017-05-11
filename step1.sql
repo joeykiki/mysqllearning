c19a55a72dd5473712b1f0479fdcef0a9b23a9c9
joeykiki <1227240139@qq.com>
2017-05-11 23:35:38 +0800

    step1修改版

--- step1.sql
+++ step1.sql
@@ -1,35 +1,39 @@
 CREATE database database3;
 
 -- 建表department，主码为dept_name
-CREATE TABLE `database3`.`department` (
-  `dept_name` VARCHAR(45) NOT NULL,
-  `building` VARCHAR(45),
-  `budget` INT UNSIGNED NULL,
-   PRIMARY KEY (`dept_name`)
-   )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `department` (
+  `dept_name` varchar(45) NOT NULL,
+  `building` varchar(45) DEFAULT NULL,
+  `budget` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`dept_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 建表student，主码为ID，外码为dept_name，参照department表中的dept_name
-CREATE TABLE `database3`.`student` (
-  `ID` INT NOT NULL,
-  `name` VARCHAR(45),
-  `sex` VARCHAR(1),
-  `age` INT(10) UNSIGNED NULL,
-  `dept_name` VARCHAR(45),
+CREATE TABLE `students` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  `age` int(10) unsigned DEFAULT NULL,
+  `emotion_state` varchar(45) DEFAULT NULL,
+  `dept_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`ID`),
-    FOREIGN KEY (`dept_name`)
-    REFERENCES `database2`.`department` (`dept_name`)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
-    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `dept_name_idx` (`dept_name`),
+  CONSTRAINT `dept_name`
+  FOREIGN KEY (`dept_name`) 
+  REFERENCES `department` (`dept_name`) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 建表exam，主码为student_ID和exam，外码为student_ID，参考student表中的ID    
-CREATE TABLE `database3`.`exam` (
-  `student_ID` INT NOT NULL,
-  `exam` VARCHAR(45) NOT NULL,
-  `grade` INT UNSIGNED NULL,
-  PRIMARY KEY (`student_ID`, `exam`),
-  CONSTRAINT `student_ID`
-    FOREIGN KEY (`student_ID`)
-    REFERENCES `database2`.`students` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE);
+CREATE TABLE `exam` (
+  `student_ID` int(11) NOT NULL,
+  `exam` varchar(45) NOT NULL,
+  `grade` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`student_ID`,`exam`),
+  CONSTRAINT `student_ID` 
+  FOREIGN KEY (`student_ID`)
+  REFERENCES `students` (`ID`) 
+  ON DELETE NO ACTION 
+  ON UPDATE CASCADE
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8
